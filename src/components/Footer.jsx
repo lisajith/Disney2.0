7062b84cefe0f20c57f99c5dce3ca42e68fd15d6
lisajith <108571827+lisajith@users.noreply.github.com>
2025-01-25 10:24:05 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='text-blue-100 flex justify-center p-10'>
-        <h1>&copy; {new Date().getFullYear()} AjithMalle</h1>
+        <h1>&copy; copyright {new Date().getFullYear()} AjithMalle</h1>
     </div>
   )
 }
